418af6065c1505d9a70b58043ddc73f29526362c
micheleguerriero <michele.guerriero2391@gmail.com>
2016-04-06 16:05:59 +0200

    Test.

--- src/main/java/dataCollector/Main.java
+++ src/main/java/dataCollector/Main.java
@@ -13,7 +13,7 @@ public class Main
     {
     	NuroApplicationDC dc=new NuroApplicationDC();
 
-    	try {
+    	  try {
     		EnvironmentReader config = EnvironmentReader.getInstance();
 
 			dc.startMonitor(config);
